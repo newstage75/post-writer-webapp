4078cfa08f384c98d663836267a82353457e4dc9
nagasawa hirotomo <hirotomo.nagasawa@gmail.com>
2024-12-27 16:28:04 +0900

    スマホサイズ用のナビゲーションメニューを作成

--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -1,11 +1,17 @@
+"use client";
+
 import { NavItem } from "@/types";
 import Link from "next/link";
+import MobileNav from "./MobileNav";
+import { ReactNode, useState } from "react";
 interface MainNavProps {
   items?: NavItem[];
   chilren?: ReactNode;
 }
 
 export default function MainNav({ items }: MainNavProps) {
+  const [showMobileMenu, setShowMobileMenu] = useState<boolean>(false);
+
   return (
     <div className="flex items-center gap-6">
       <Link href={"/"} className="hidden md:flex items-center space-x-2">
@@ -22,6 +28,13 @@ export default function MainNav({ items }: MainNavProps) {
           </Link>
         ))}
       </nav>
+      <button
+        className="md:hidden flex items-center"
+        onClick={() => setShowMobileMenu(!showMobileMenu)}
+      >
+        <span>メニュー</span>
+      </button>
+      {showMobileMenu && <MobileNav />}
     </div>
   );
 }
---
+++ components/MobileNav.tsx
@@ -0,0 +1,9 @@
+// ednfのタブ保管で「デフォルトエクスポートの関数コンポーネントの雛形を自動補完」
+
+export default function MobileNav() {
+  return (
+    <div>
+      <div>MobileNav</div>
+    </div>
+  );
+}
