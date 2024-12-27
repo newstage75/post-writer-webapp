9d07abb7d53204311ef369cf4f3cd0e3d73a85a8
nagasawa hirotomo <hirotomo.nagasawa@gmail.com>
2024-12-27 19:23:45 +0900

    featureセクションの作成

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -39,6 +39,20 @@ export default function IndexPage() {
           </div>
         </div>
       </section>
+
+      <section
+        id="features"
+        className="container py-8 md:pd-12 lg:py-24 bg-slate-50"
+      >
+        <div className="text-center space-y-6">
+          <h2 className="font-extrabold text-3xl md:text-6xl">
+            サービスの特徴
+          </h2>
+          <p className="text-muted-foreground sm:txt-lg sm:leading-7">
+            このプロジェクトはモダンな技術スタックを使って作られたWebアプリケーションです。Next.jsAppRouterやcontentlayerを利用してマークダウン形式でブログ投稿ができます。
+          </p>
+        </div>
+      </section>
     </>
   );
 }
