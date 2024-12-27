b7e18a752fbeea7f348777e068b0f79e622dc2a1
nagasawa hirotomo <hirotomo.nagasawa@gmail.com>
2024-12-27 12:39:19 +0900

    css調整

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -5,9 +5,16 @@ export default function IndexPage() {
     <>
       <section className="pt-6 md:pt-10 lg:py-32 pb-8 md:pb-12">
         <div className="container text-center flex flex-col items-center gap-4 max-w-[64rem] mx-auto">
-          <Link href={"/"}>Xをフォローする</Link>
-          <h1>Post Witerr</h1>
-          <p>
+          <Link
+            href={"/"}
+            className="bg-muted px-4 py-1.5 rounded-md font-medium text-sm"
+          >
+            Xをフォローする
+          </Link>
+          <h1 className="font-extrabold text-3xl sm:text-5xl md:text-6xl lg:text-7xl">
+            Post Writer
+          </h1>
+          <p className="text-muted-foreground sm:text-xl leading-normal max-w-[42rem]">
             このアプリケーションはNext.js
             AppRouterで作られたものです。ユーザーは自由に投稿をポストすることができます。
           </p>
