36a8e6df0b9c19b728ff7c16ece97eb3ca023005
nagasawa hirotomo <hirotomo.nagasawa@gmail.com>
2024-12-27 12:29:54 +0900

    ルートグループの作成とHomeページの作成

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+export default function IndexPage() {
+  return (
+    <>
+      <section className="pt-6 md:pt-10 lg:py-32 pb-8 md:pb-12">
+        <div className="container text-center flex flex-col items-center gap-4 max-w-[64rem] mx-auto">
+          <Link href={"/"}>Xをフォローする</Link>
+          <h1>Post Witerr</h1>
+          <p>
+            このアプリケーションはNext.js
+            AppRouterで作られたものです。ユーザーは自由に投稿をポストすることができます。
+          </p>
+        </div>
+      </section>
+    </>
+  );
+}
